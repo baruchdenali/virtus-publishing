ded7932bd52fa409eecb51e8e067ef662622d1b9
Baruch Denali <Baruch.Denali@gmail.com>
2026-06-19 11:47:43 +0800

    test: force Node.js runtime for test function

--- api/test.js
+++ api/test.js
@@ -1,6 +1,10 @@
-// Simple Vercel function test - Node.js style
+// Simple Vercel function test - force Node.js runtime
+export const config = {
+  runtime: 'nodejs18.x',
+};
+
 export default function handler(req, res) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ ok: true, msg: "Function works!" }));
+  res.end(JSON.stringify({ ok: true, msg: "Node.js function works!" }));
 }
